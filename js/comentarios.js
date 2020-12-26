1d6ac1a9f56f6d4b9fc4d01814ab3668d9c03cda
Wendel Santana <wendelsantana0202@gmail.com>
2020-12-25 21:02:22 -0300

    first commit

---
+++ js/comentarios.js
@@ -0,0 +1,11 @@
+// Aqui eu tenho um comentário em linha 
+
+/*
+
+    console.log("Bem vindos ao Starter!")
+
+    o código abaixo escreve alguma mensagem no devtools 
+
+*/
+
+console.log('Bem vindos ao Starter!') // alguma coisa console.log('Iria ignorar')
---
+++ js/dist/tiposDeDados.dev.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Tipos de dados
+
+/* Gramática
+    * Elementos da linguagem e suas cominações
+    * A arte de falar e escrever corretamente
+*/
+
+/* Vocabulário
+    * Conjunto de termos e expressões
+    * Agrupamento de palavras
+*/
+// Precisamos saber escrever
+// Precisamos saber os significados
+// Precisamos continuar aprendendo, para crescer nosso vocabulário.
+// Como vai ser a dinâmica do aprendizado?
+// Conceitos e escrita
+
+/* => Vamos aprender os tipos de dados mais utilizados na linguagem
+    * ??? Voce sabia que possível aprender 80% de uma lingua nova, com cerca de 20% do vocabulário? (até menos...)  
+*/
+
+/*  String
+
+ * Cadeia de caracteres
+
+    ' ' => aspas simples 
+    " " => aspas duplas
+    ` ` => template literals ou templates strings 
+
+
+*/
+console.log("");
