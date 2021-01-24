9768b64c0a069890af86726d3ee19b290365ad88
Wendel Santana <wendelsantana0202@gmail.com>
2021-01-24 20:24:19 -0300

    new words

--- js/manipulandoDados/dist/separandoStrings.dev.js
+++ js/manipulandoDados/dist/separandoStrings.dev.js
@@ -3,6 +3,8 @@
 // Manipulando Strings e Arrays
 // Separe um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
 var phrase = "Eu quero viver o Amor!";
-var myArray = phrase.split(" ");
-var phraseWithUnderScore = MyArray.join("_");
+var myArray = phrase.split(" "); // o split troca o que for colocado como argumento
+
+var phraseWithUnderScore = MyArray.join("_"); // o join vai junat o for colocado como argumento
+
 console.log(phraseWithUnderScore);
--- js/manipulandoDados/separandoStrings.js
+++ js/manipulandoDados/separandoStrings.js
@@ -3,6 +3,6 @@
 // Separe um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
 
 let phrase = "Eu quero viver o Amor!"
-let myArray = phrase.split(" ")
-let phraseWithUnderScore = MyArray.join("_")
+let myArray = phrase.split(" ") // o split troca o que for colocado como argumento
+let phraseWithUnderScore = MyArray.join("_") // o join vai junat o for colocado como argumento
 console.log(phraseWithUnderScore)
