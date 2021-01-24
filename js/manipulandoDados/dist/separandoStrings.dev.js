182933ca2eb9b9263344ff17d13648bafbb78f93
Wendel Santana <wendelsantana0202@gmail.com>
2021-01-24 20:11:45 -0300

    ok

---
+++ js/manipulandoDados/dist/separandoStrings.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Manipulando Strings e Arrays
+// Separe um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
+var phrase = "Eu quero viver o Amor!";
+var myArray = phrase.split(" ");
+var phraseWithUnderScore = MyArray.join("_");
+console.log(phraseWithUnderScore);
